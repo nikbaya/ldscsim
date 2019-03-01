1801338c538b3ff1f0647cd7d0b10aebea3ca531
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2019-03-01 08:47:30 -0500

    Update sim_correlated_phens.py

--- other_versions/sim_correlated_phens.py
+++ other_versions/sim_correlated_phens.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Feb 12 11:27:04 2019
 
-Generate correlated betas
+Generate correlated betas for any number of phenotypes
 
 @author: nbaya
 """
@@ -13,12 +13,6 @@ import numpy as np
 import scipy.stats as stats
 import pandas as pd
 
-
-"""
-*******************************
-n correlated phenotype solution
-*******************************
-"""
 def sim_correlated_phens(mt, genotype, h2_ls=[], rg_ls=[], cov_array = None, seed = None):
     if type(h2_ls) is not list or type(rg_ls) is not list:
         raise ValueError("Both h2_ls and rg_ls must be lists")
@@ -47,8 +41,9 @@ def sim_correlated_phens(mt, genotype, h2_ls=[], rg_ls=[], cov_array = None, see
 def create_cov_array(h2_ls, rg_ls):
     n_rg = len(rg_ls)
     n_h2 = len(h2_ls)
-    if n_rg is not int((n_h2**2-n_h2)/2):
-        raise ValueError("The number of rg values given is {}, expected {}".format(n_rg,int((n_h2**2-n_h2)/2)))
+    exp_n_rg = int((n_h2**2-n_h2)/2)
+    if n_rg is not exp_n_rg:
+        raise ValueError(f'The number of rg values given is {n_rg}, expected {exp_n_rg}')
     rg_ls = np.asarray(rg_ls)
     cov_array = np.zeros(shape=[n_h2,n_h2])
     cov_array[np.triu_indices(n_h2, k=1)] = rg_ls**2 #sets upper diagonal with covariances
@@ -68,17 +63,18 @@ def create_correlated_betas(mt, cov_array, seed):
     randstate = np.random.RandomState(int(seed)) #seed random state for replicability
     if n_phens > 1:
         betas = randstate.multivariate_normal(mean=np.zeros(n_phens),cov=cov_array,size=[M,])
-    df = pd.DataFrame(betas,columns=['beta_{}'.format(i) for i in range(n_phens)])
+    df = pd.DataFrame(betas,columns=[f'beta_{i}' for i in range(n_phens)])
     tb = hl.Table.from_pandas(df)
     tb = tb.add_index().key_by('idx')
     mt = mt.add_row_index()
-    return mt.annotate_rows(betas = tb[mt.row_idx]) 
-   
+    for i in range(n_phens):
+        mt = mt._annotate_all(row_exprs = {f'beta_{i}': tb[mt.row_idx][{f'beta_{i}'}]}) 
+
 def sim_phenotypes(mt, cov_array):
     h2_ls = np.diagonal(cov_array)
     n_phens = len(h2_ls)
     for beta_i in range(n_phens):
-        mt = mt._annotate_all(col_exprs={'__y_no_noise_{}'.format(beta_i): hl.agg.sum(mt.betas['beta_{}'.format(beta_i)] * mt.__norm_gt)})
+        mt = mt._annotate_all(col_exprs={f'__y_no_noise_{beta_i}': hl.agg.sum(mt.betas[f'beta_{beta_i}'] * mt.__norm_gt)})
     for beta_i in range(n_phens):
-        mt = mt._annotate_all(col_exprs={'__y_{}'.format(beta_i):mt['__y_no_noise_{}'.format(beta_i)]+hl.rand_norm(0,hl.sqrt(1-h2_ls[beta_i]))})
+        mt = mt._annotate_all(col_exprs={f'__y_{beta_i}':mt[f'__y_no_noise_{beta_i}']+hl.rand_norm(0,hl.sqrt(1-h2_ls[beta_i]))})
     return mt
